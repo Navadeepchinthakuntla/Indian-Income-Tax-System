50625f66feca81d3140d750ce9a25d91a07d6d31
NavadeepChinthakuntla <chinthakuntlanavadeep@gmail.com>
2026-03-15 12:20:59 +0530

    day 3 :implemented tax calculation logic

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         TAXPAYER person =new TAXPAYER();
-        person.name="ajay";
-        person.age=22;
-        person.employmentType="private";
-        person.annualincome=600000;
+        System.out.print("Enter Person Name : ");
+        person.name=sc.nextLine();
+        System.out.print("Enter Person Age : ");
+        person.age= sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Employment type : ");
+        person.employmentType= sc.nextLine();
+        System.out.print("Enter Annual Income : ");
+        person.annualincome= sc.nextDouble();
+        double tax=TaxCalculator.CalculateTax(person.annualincome);
+        System.out.println("\n-----TAX REPORT-----");
         System.out.println("Name : "+person.name);
         System.out.println("Age : "+person.age);
         System.out.println("Employment type : "+person.employmentType);
-        System.out.println("Annulincome : "+person.annualincome);
+        System.out.println("Annul Income : "+person.annualincome);
+        System.out.println("Tax Payable : "+tax);
 
 
     }
